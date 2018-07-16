05aaa28dea7025a31b0b7938d09109484066372c
unknown <b8emir@gmail.com>
2018-07-16 12:12:32 +0200

    First commit

---
+++ ActivityTApp/src/main/java/com/team4/app/ActivityTAppApplication.java
@@ -0,0 +1,12 @@
+package com.team4.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivityTAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActivityTAppApplication.class, args);
+    }
+}
